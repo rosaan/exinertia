6e3eb406a2d42953c788480697316358ee96caa6
assim <hello@assim.me>
2025-02-06 12:21:48 +0100

    package ready for publishing on hex

--- mix.exs
+++ mix.exs
@@ -7,21 +7,41 @@ defmodule Exinertia.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
+      description:
+        "A toolkit for seamlessly integrating Inertia.js with Phoenix, using Bun for JavaScript and CSS bundling",
+      package: package(),
+      source_url: "https://github.com/nordbeam/exinertia",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:igniter, "~> 0.5"}
+      {:igniter, "~> 0.5"},
+      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Assim El Hammouti"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nordbeam/exinertia"},
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE.md)
     ]
   end
 end

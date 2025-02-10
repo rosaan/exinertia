23ba5a3f24b1a392f216fb61f2da479237d5a3b2
assim <hello@assim.me>
2025-02-10 14:38:01 +0100

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exinertia.MixProject do
   def project do
     [
       app: :exinertia,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
